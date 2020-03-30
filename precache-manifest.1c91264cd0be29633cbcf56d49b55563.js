29d2958e01288ec7db9d191f4d9b39f19af16d4b
Vladimir <zaicevvv1991@gmail.com>
2020-03-30 13:24:14 +0300

    Updates

---
+++ precache-manifest.1c91264cd0be29633cbcf56d49b55563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af9dd00414866d073251c1e36c3b2a2",
+    "url": "/status/index.html"
+  },
+  {
+    "revision": "7ac09d3a11957e426afc",
+    "url": "/status/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "054d9de4bc97efca1846",
+    "url": "/status/static/js/2.10567837.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/status/static/js/2.10567837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac09d3a11957e426afc",
+    "url": "/status/static/js/main.8744b5ab.chunk.js"
+  },
+  {
+    "revision": "58c4067cfa76883f5617",
+    "url": "/status/static/js/runtime-main.30ea51bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/status/static/media/logo.5d5d9eef.svg"
+  }
+]);
