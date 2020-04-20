cb9d9c80db43f9deaafe68724e4487a9ee66df1e
Vladimir <zaicevvv1991@gmail.com>
2020-04-20 17:00:21 +0300

    Updates

---
+++ precache-manifest.2464f6e2ca0c1541567847291f342e50.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d54e78c2f3beaa42334f80c40d47c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db788b2994be6e0b026d",
+    "url": "/static/css/home-page.4e70f9bb.chunk.css"
+  },
+  {
+    "revision": "e0806aaebc1c63f04f66",
+    "url": "/static/css/main.8da8c149.chunk.css"
+  },
+  {
+    "revision": "dcc829614ab02b9be9d2",
+    "url": "/static/css/options-page.2086c197.chunk.css"
+  },
+  {
+    "revision": "e289d13ab2b673e1729a",
+    "url": "/static/js/4.839a782f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/4.839a782f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db788b2994be6e0b026d",
+    "url": "/static/js/home-page.064c7683.chunk.js"
+  },
+  {
+    "revision": "e0806aaebc1c63f04f66",
+    "url": "/static/js/main.ae7110cb.chunk.js"
+  },
+  {
+    "revision": "dcc829614ab02b9be9d2",
+    "url": "/static/js/options-page.98c7d8b8.chunk.js"
+  },
+  {
+    "revision": "9ff892d61ec2c39993a3",
+    "url": "/static/js/runtime-main.17ee99be.js"
+  },
+  {
+    "revision": "2dde8e0d3f9582171c14fb0900134fc4",
+    "url": "/static/media/dvig.2dde8e0d.jpg"
+  },
+  {
+    "revision": "b321c771e0653267a80cd654f0331c6e",
+    "url": "/static/media/gbc.b321c771.jpg"
+  },
+  {
+    "revision": "07af9d66f6dae7a2061510601ffb0e15",
+    "url": "/static/media/gbo.07af9d66.jpg"
+  },
+  {
+    "revision": "ee4102f1faf28dcb2b5498e1784e5d50",
+    "url": "/static/media/hod.ee4102f1.webp"
+  },
+  {
+    "revision": "94d9b0b786b76a5dd07b2ba58e6e96eb",
+    "url": "/static/media/main.94d9b0b7.jpg"
+  },
+  {
+    "revision": "d5090a5568c3e02d9d6ad3a932bf8a62",
+    "url": "/static/media/tires.d5090a55.jpg"
+  },
+  {
+    "revision": "e4b82284eb43936e34fb685a178f610f",
+    "url": "/static/media/tnvd.e4b82284.jpg"
+  },
+  {
+    "revision": "e5c98cdcd778163176bfe83c5caec319",
+    "url": "/static/media/turbo.e5c98cdc.jpg"
+  }
+]);
