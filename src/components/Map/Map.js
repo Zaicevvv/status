b2c7d5c798c83f9428f706b1bc5071cf4c9eed09
Vladimir <zaicevvv1991@gmail.com>
2020-05-01 01:04:26 +0300

    alm

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,14 +3,11 @@ import css from './Map.module.css';
 
 const Map = () => (
   <div className={css.mapWrap}>
-    <img
-      className={css.moveIcon}
-      src="https://img.icons8.com/ultraviolet/40/000000/collect.png"
-    />
-    <a
-      className={css.move}
-      href="https://maps.google.com/maps?ll=50.440528,30.406935&z=19&t=m&hl=ru&gl=UA&mapclient=embed&daddr=%D0%A0%D0%B5%D0%B7%D0%B8%D0%BD%D0%B0%20%D0%B1%2F%D1%83%20%D0%9B%D0%B5%D0%BF%D1%81%D0%B5%2C%D1%81%D0%BA%D0%BB%D0%B0%D0%B4%20(%D0%92%D0%B0%D1%86%D0%BB%D0%B0%D0%B2%D0%B0%20%D0%93%D0%B0%D0%B2%D0%B5%D0%BB%D0%B016)%20%D0%B7%D0%B0%D0%B5%D0%B7%D0%B4%20%D1%81%20%D1%83%D0%BB%D0%B8%D1%86%D1%8B%20%D0%9A%D0%B0%D0%B1%D0%BB%D1%83%D0%BA%D0%BE%D0%B2%D0%B0%2026%2C%2003126%20%D0%B1%D1%83%D0%BB%D1%8C%D0%B2%D0%B0%D1%80%20%D0%92%D0%B0%D1%86%D0%BB%D0%B0%D0%B2%D0%B0%20%D0%93%D0%B0%D0%B2%D0%B5%D0%BB%D0%B0%2C%2016%20%D0%9A%D0%B8%D1%97%D0%B2@50.4405279,30.4069347"
-    >
+    <a className={css.move} href="https://goo.gl/maps/TtBF5GJ2PyjSnxMZ6">
+      <img
+        className={css.moveIcon}
+        src="https://img.icons8.com/ultraviolet/40/000000/collect.png"
+      />
       Маршрут
     </a>
     <iframe
